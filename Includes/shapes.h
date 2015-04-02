a83913ae7097b25d2ac556aac76a89c6c4955c2b
ronak <ronak.super@gmail.com>
2015-04-02 23:04:39 +0530

    New shapes added

--- Includes/shapes.h
+++ Includes/shapes.h
@@ -10,15 +10,16 @@ void drawline( bitmap *image, pixel **p, int x0, int x1, int y0, int y1);
 //Function draws the line with given color
 void drawlinec( bitmap *image, pixel **p, int x0, int x1, int y0, int y1, pixel color);
 
-void drawcirlce(bitmap*image, pixel **p, int x_center, int y_center, int r);
+void drawcircle(bitmap*image, pixel **p, int x_center, int y_center, int r);
 
 static void plot(bitmap *image, pixel **p, int x, int y, pixel color);
 
 void fill_circle(bitmap* image, pixel **p, int xc, int yc, int r, pixel color);
 
 
-void drawcirlcec(bitmap* image, pixel **p, int xc, int yc, int r, pixel color_black);
+void drawcirclec(bitmap* image, pixel **p, int xc, int yc, int r, pixel color_black);
 
-//Rectangle draw
-void drawrect(bitmap* image, pixel **p, int length, int breadth, int x, int y);
+void fillrect(bitmap *image, pixel **p, int length, int breadth, int x, int y, pixel color);
+
+void drawrect(bitmap *image, pixel **p, int length, int breadth, int x, int y, pixel color);
 #endif
